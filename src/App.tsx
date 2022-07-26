bc8c67774ac631e726bd4ab3e3e42bb5329617d0
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2022-07-26 21:57:45 +0300

    Samurai_way_Lesson_19_complete

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,32 @@ import './App.css';
 import {Header} from "./component/Header/Header";
 import {Navbar} from "./component/Navbar/Navbar";
 import {Profile} from "./component/Profile/Profile";
-// import {BrowserRouter, Route} from "react-router-dom";
+import {Dialogs} from './component/Dialogs/Dialogs'
+import {BrowserRouter, Route} from "react-router-dom";
+import {News} from "./component/News/News";
+import {Music} from "./component/Music/Music";
+import {Settings} from "./component/Settings/Setting";
 // import Technologies from "./component/technologies";
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
-            <Header />
-            <Navbar />
-            <Profile />
-        </div>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className="app-wrapper-appContent">
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/>
+
+                    {/*<Dialogs />*/}
+                    {/*<Profile />*/}
+                </div>
+
+            </div>
+        </BrowserRouter>
     );
 }
 
--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -4,11 +4,11 @@ import n from './Navbar.module.css'
 export const Navbar = () => {
     return (
         <nav className={n.appNav}>
-            <div><a className={n.item} href="src/component/Navbar/Navbar#">Profile</a></div>
-            <div><a  className={n.item} href='src/component/Navbar/Navbar#'>Messages</a></div>
-            <div><a className={n.item} href='src/component/Navbar/Navbar#'>News</a></div>
-            <div><a className={n.item} href='src/component/Navbar/Navbar#'>Music</a></div>
-            <div><a className={n.item} href='src/component/Navbar/Navbar#'>Settings</a></div>
+            <div><a className={n.item} href="/profile">Profile</a></div>
+            <div><a className={n.item} href='/dialogs'>Messages</a></div>
+            <div><a className={n.item} href='/news'>News</a></div>
+            <div><a className={n.item} href='/music'>Music</a></div>
+            <div><a className={n.item} href='/settings'>Settings</a></div>
         </nav>
     )
 }
